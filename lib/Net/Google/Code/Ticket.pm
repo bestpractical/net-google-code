109d6d7335638d5e4421296973ea450d10e3b81a
sunnavy <sunnavy@bestpractical.com>
2008-12-26 08:24:08 +0000

    parse metadata and labels

--- lib/Net/Google/Code/Ticket.pm
+++ lib/Net/Google/Code/Ticket.pm
@@ -16,6 +16,13 @@ has connection => (
 has state => (
     isa => 'HashRef',
     is  => 'rw',
+    default => sub { {} },
+);
+
+has labels => (
+    isa => 'HashRef',
+    is  => 'rw',
+    default => sub { {} },
 );
 
 has comments => (
@@ -23,7 +30,7 @@ has comments => (
     is => 'rw',
 );
 
-our @PROPS = qw(status owner closed cc summary);
+our @PROPS = qw(status owner closed cc summary description);
 
 for my $prop (@PROPS) {
     no strict 'refs'; ## no critic
@@ -40,14 +47,54 @@ sub load {
     $self->connection->_fetch( "/issues/detail?id=" . $id );
 
     my $content = $self->connection->mech->content;
+    require HTML::TreeBuilder;
+    my $tree    = HTML::TreeBuilder->new;
+    $tree->parse_content($content);
+    $tree->elementify;
+    my ($meta) = $tree->look_down( id => 'issuemeta' );
+    my @meta = $meta->find_by_tag_name('tr');
+    for my $meta (@meta) {
 
-    my $stateref;
-    #XXX scrap $content to get state and comments
+        my ( $key, $value );
+        if ( my $k = $meta->find_by_tag_name('th') ) {
+            my $v         = $meta->find_by_tag_name('td');
+            my $k_content = $k->content_array_ref->[0];
+            while ( ref $k_content ) {
+                $k_content = $k_content->content_array_ref->[0];
+            }
+            $key = $k_content;    # $key is like 'Status:#'
+            $key =~ s/:.$//;      # s/:#$// doesn't work, no idea why
 
-    return unless $stateref;
-    $self->state( $stateref->{$id} );
+            if ($v) {
+                my $v_content = $v->content_array_ref->[0];
+                while ( ref $v_content ) {
+                    $v_content = $v_content->content_array_ref->[0];
+                }
+                $value = $v_content;
+                $value =~ s/^\s+//;
+                $value =~ s/\s+$//;
+            }
+            $self->state->{lc $key} = $value;
+        }
+        else {
+            my $href = $meta->find_by_tag_name('a')->attr_get_i('href');
+
+# from issue tracking faq:
+# The prefix before the first dash is the key, and the part after it is the value.
+            if ( $href =~ /list\?q=label:([^-]+?)-(.+)/ ) {
+                ( $key, $value ) = ( $1, $2 );
+            }
+            elsif ( $href =~ /list\?q=label:([^-]+)$/ ) {
+                $key = $1;
+            }
+            else {
+                warn "can't parse label from $href";
+            }
+            $self->labels->{$key} = $value;
+        }
+    }
+
     return $id;
-
 }
 
 sub _fetch_new_ticket_metadata {
