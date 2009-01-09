75af268fc9a95dc6bf21a482bd3faf9182c9ecf6
Fayland <fayland@gmail.com>
2009-01-09 06:10:14 +0000

    make t\pod-coverage happy

--- lib/Net/Google/Code/Home.pm
+++ lib/Net/Google/Code/Home.pm
@@ -81,4 +81,21 @@ __PACKAGE__->meta->make_immutable;
 1;
 __END__
 
-# Fayland Lam, C<< <fayland at gmail.com> >>
+=head1 NAME
+
+Net::Google::Code::Home - 
+
+=head1 DESCRIPTION
+
+used by L<Net::Google::Code>
+
+=head1 AUTHOR
+
+Fayland Lam, C<< <fayland at gmail.com> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright 2008-2009 Best Practical Solutions.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
