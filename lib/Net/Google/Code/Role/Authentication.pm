c9f2464e31d8a085c08a0f413b27ffc4c62cde91
sunnavy <sunnavy@bestpractical.com>
2009-02-09 06:48:41 +0000

    improvement authentication

--- lib/Net/Google/Code/Role/Authentication.pm
+++ lib/Net/Google/Code/Role/Authentication.pm
@@ -16,11 +16,21 @@ has 'password' => (
 
 sub signin {
     my $self = shift;
+    return 1 if $self->signed_in;
     $self->ask_password unless $self->password && length $self->password;
 
-    $self->mech->follow_link(
-        url_regex => qr!^https?://www\.google\.com/accounts/Login! )
-      || $self->mech->get('https://www.google.com/accounts/Login');
+    my $already_in_google;
+    if (
+        $self->mech->follow_link(
+            url_regex => qr!^https?://www\.google\.com/accounts/Login!
+        )
+      )
+    {
+        $already_in_google = 1;
+    }
+    else {
+        $self->mech->get('https://www.google.com/accounts/Login');
+    }
 
     $self->mech->submit_form(
         with_fields => {
@@ -30,7 +40,8 @@ sub signin {
     );
 
     die 'signin failed to google code'
-      unless $self->mech->content =~ m!Sign Out!;
+      unless ( $already_in_google && $self->mech->uri =~ /CheckCookie/ )
+      || ( !$already_in_google && $self->mech->content =~ /Sign Out/ );
 
     return 1;
 }
@@ -62,7 +73,8 @@ sub ask_password {
 sub signed_in {
     my $self = shift;
     return 1
-      if $self->mech->uri =~ m!https?://[-\w]+\.google\.com/!
+      if $self->mech->content
+          && $self->mech->uri =~ m!https?://[-\w]+\.google\.com/!
           && $self->mech->content =~ /Sign Out/;
     return;
 }
