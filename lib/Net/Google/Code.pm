c94e4095a3678a421d049657d46ae657dcbc8e13
sunnavy <sunnavy@bestpractical.com>
2009-05-14 08:05:52 +0000

    update version to 0.05

--- lib/Net/Google/Code.pm
+++ lib/Net/Google/Code.pm
@@ -4,7 +4,7 @@ use Moose;
 with 'Net::Google::Code::Role::Fetchable', 'Net::Google::Code::Role::URL',
   'Net::Google::Code::Role::Pageable';
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 has 'project' => (
     isa      => 'Str',
