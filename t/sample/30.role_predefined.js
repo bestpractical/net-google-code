da6aafc775edc25ad94773d34d9092b71238b366
sunnavy <sunnavy@bestpractical.com>
2009-05-29 03:17:35 +0000

    tests for predefined role

---
+++ t/sample/30.role_predefined.js
@@ -0,0 +1,132 @@
+{ 'open': [
+ 
+ { name:"New",
+ doc:"Issue has not had initial review yet" },
+ 
+ { name:"Accepted",
+ doc:"Problem reproduced / Need acknowledged" },
+ 
+ { name:"Started",
+ doc:"Work on this issue has begun" }
+ 
+ ],
+ 'closed': [
+ 
+ { name:"Fixed",
+ doc:"Developer made source code changes, QA should verify" },
+ 
+ { name:"Verified",
+ doc:"QA has verified that the fix worked" },
+ 
+ { name:"Invalid",
+ doc:"This was not a valid issue report" },
+ 
+ { name:"Duplicate",
+ doc:"This report duplicates an existing issue" },
+ 
+ { name:"WontFix",
+ doc:"We decided to not take action on this issue" },
+ 
+ { name:"Done",
+ doc:"The requested non-coding task was completed" }
+ 
+ ],
+ 'statuses_offer_merge': [
+ "Duplicate"
+ ],
+ 'labels': [
+ 
+ { name:"Type-Defect",
+ doc:"Report of a software defect" },
+ 
+ { name:"Type-Enhancement",
+ doc:"Request for enhancement" },
+ 
+ { name:"Type-Task",
+ doc:"Work item that doesn&#39;t change the code or docs" },
+ 
+ { name:"Type-Review",
+ doc:"Request for a source code review" },
+ 
+ { name:"Type-Other",
+ doc:"Some other kind of issue" },
+ 
+ { name:"Priority-Critical",
+ doc:"Must resolve in the specified milestone" },
+ 
+ { name:"Priority-High",
+ doc:"Strongly want to resolve in the specified milestone" },
+ 
+ { name:"Priority-Medium",
+ doc:"Normal priority" },
+ 
+ { name:"Priority-Low",
+ doc:"Might slip to later milestone" },
+ 
+ { name:"OpSys-All",
+ doc:"Affects all operating systems" },
+ 
+ { name:"OpSys-Windows",
+ doc:"Affects Windows users" },
+ 
+ { name:"OpSys-Linux",
+ doc:"Affects Linux users" },
+ 
+ { name:"OpSys-OSX",
+ doc:"Affects Mac OS X users" },
+ 
+ { name:"Milestone-Release1.0",
+ doc:"All essential functionality working" },
+ 
+ { name:"Component-UI",
+ doc:"Issue relates to program UI" },
+ 
+ { name:"Component-Logic",
+ doc:"Issue relates to application logic" },
+ 
+ { name:"Component-Persistence",
+ doc:"Issue relates to data storage components" },
+ 
+ { name:"Component-Scripts",
+ doc:"Utility and installation scripts" },
+ 
+ { name:"Component-Docs",
+ doc:"Issue relates to end-user documentation" },
+ 
+ { name:"Security",
+ doc:"Security risk to users" },
+ 
+ { name:"Performance",
+ doc:"Performance issue" },
+ 
+ { name:"Usability",
+ doc:"Affects program usability" },
+ 
+ { name:"Maintainability",
+ doc:"Hinders future changes" }
+ 
+ ],
+ 'excl_prefixes': [
+ 
+ "type",
+ 
+ "priority",
+ 
+ "milestone"
+ 
+ ],
+ 'members': [
+ 
+ { name:"sunnavy",
+ doc:"" }
+ ,
+ 
+ { name:"jessev",
+ doc:"" },
+ 
+ { name:"fayland",
+ doc:"" }
+ 
+ ],
+ 'strict': 0
+}
