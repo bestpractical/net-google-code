c6402eb54f615cfdc9b3818d2ea83d95d020ffb5
sunnavy <sunnavy@bestpractical.com>
2009-06-23 01:48:41 +0000

    update version to 0.13

--- lib/Net/Google/Code.pm
+++ lib/Net/Google/Code.pm
@@ -4,7 +4,7 @@ use Any::Moose;
 with 'Net::Google::Code::TypicalRoles';
 use Scalar::Util qw/blessed/;
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 has 'project' => (
     isa      => 'Str',
