58d2b2f5b8f3194bd5721d6476d629d3c3c3c743
sunnavy <sunnavy@bestpractical.com>
2010-01-29 13:23:19 +0800

    update copyright: happy new year

--- lib/Net/Google/Code/Issue/Search.pm
+++ lib/Net/Google/Code/Issue/Search.pm
@@ -195,7 +195,7 @@ sunnavy  C<< <sunnavy@bestpractical.com> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright 2008-2009 Best Practical Solutions.
+Copyright 2008-2010 Best Practical Solutions.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
