0bcb21a0d5034be4d5c7507d9547a5deaeda1386
sunnavy <sunnavy@bestpractical.com>
2010-04-29 06:03:25 +0800

    bump version to 0.20

--- lib/Net/Google/Code.pm
+++ lib/Net/Google/Code.pm
@@ -4,7 +4,7 @@ use Any::Moose;
 with 'Net::Google::Code::TypicalRoles';
 use Scalar::Util qw/blessed/;
 
-our $VERSION = '0.19';
+our $VERSION = '0.20';
 
 has 'project' => (
     isa      => 'Str',
